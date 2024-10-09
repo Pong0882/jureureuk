9ed3a007edd90f2ba59416ab8d336eeef61b730f
Pong <parkdanil@naver.com>
2024-10-09 17:35:46 +0900

    add spring 10/09 17:35

---
+++ jureureuk/src/main/java/com/jureureuk/jureureuk/JureureukApplication.java
@@ -0,0 +1,13 @@
+package com.jureureuk.jureureuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JureureukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JureureukApplication.class, args);
+	}
+
+}
---
+++ jureureuk/src/test/java/com/jureureuk/jureureuk/JureureukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jureureuk.jureureuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JureureukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
